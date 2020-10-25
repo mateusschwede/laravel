ff1cef1081aaae4fe02550f8c6001c3edd02d180
mateusschwede <mateus.schwede@hotmail.com>
2020-10-25 13:32:33 -0300

    Arquivos organizados e aula2 finalizada

--- projeto1/app/Http/Controllers/UserController.php
+++ projeto1/app/Http/Controllers/UserController.php
@@ -1,9 +1,22 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash; //Import Hash para senhas
+use App\Models\User; //Import Model User.php
 
 class UserController extends Controller {
     public function listUser() {
-        echo "Listar Users";
+        /*$user = new User();
+        $user->name = 'Mateus';
+        $user->email = 'mateus1908.schwede@gmail.com';
+        $user->password = Hash::make('123');
+        $user->save();*/
+
+        //select user where id=1 somente o 1º que encontrar, abaixo
+        $user = User::where('id','=',1)->first();
+        return view('listUser',[
+            'user' => $user
+            //var visao => var controller
+        ]);
     }
 }
---
+++ projeto1/resources/views/listUser.blade.php
@@ -0,0 +1,3 @@
+<h1>Listagem Users</h1>
+<p>Nome: {{ $user->name }}</p>
+<p>Email: {{ $user->email }}</p>
